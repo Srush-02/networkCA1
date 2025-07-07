6ae76673c32773752560cb0a0695d3af8fddaa06
srushti <srushtiraut97@gmail.com>
2025-07-07 12:39:20 +0100

    checks

--- restApi/src/main/java/restApi/restApi/controller/Controller.java
+++ restApi/src/main/java/restApi/restApi/controller/Controller.java
@@ -1,38 +1,33 @@
 package restApi.restApi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.Arrays;
 import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import restApi.restApi.entity.Patient;
 
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class Controller {
 	
 	@GetMapping("/")
 	public String getData() {
 		return "Testing AWS EC2 file";
 	}
-	
-
-
-
-public class PatientController {
 
     @GetMapping("/patients")
-    public ResponseEntity<Response> getAllPatients() {
-        List<Patient> patientList = Arrays.asList(
-            new Patient("John Doe", "2025-07-01", "Complete", "1234567890", "john@example.com", "Blood Test", "2025-07-10"),
-            new Patient("Jack Smith", "2025-06-26", "Pending", "7876543210", "jack@example.com", "X-Ray Test", "2025-07-08")
-        );
-
-        return new ResponseEntity<>(new Response("success", patientList, null), HttpStatus.OK);
+    public List<Patient> getAllPatients() {
+    	return Arrays.asList(
+                new Patient("John Doe", "2025-07-01", "Complete", "1234567890", "john@example.com", "Blood Test", "2025-07-10"),
+                new Patient("Jack", "2025-06-26", "Pending", "9251478200", "jack@example.com", "X-Ray Test", "2025-07-08")
+            );
     }
-}
+
 
 }
---
+++ restApi/src/main/java/restApi/restApi/entity/Patient.java
@@ -0,0 +1,81 @@
+package restApi.restApi.entity;
+
+public class Patient {
+    private String first_name;
+    private String date_of_birth;
+    private String gender; 
+    private String phone_number;
+    private String patient_email;
+    private String test_name;
+    private String appointment_date;
+
+    public Patient(String first_name, String date_of_birth, String gender, String phone_number, 
+                   String patient_email, String test_name, String appointment_date) {
+        this.first_name = first_name;
+        this.date_of_birth = date_of_birth;
+        this.gender = gender;
+        this.phone_number = phone_number;
+        this.patient_email = patient_email;
+        this.test_name = test_name;
+        this.appointment_date = appointment_date;
+    }
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getDate_of_birth() {
+		return date_of_birth;
+	}
+
+	public void setDate_of_birth(String date_of_birth) {
+		this.date_of_birth = date_of_birth;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getPhone_number() {
+		return phone_number;
+	}
+
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+
+	public String getPatient_email() {
+		return patient_email;
+	}
+
+	public void setPatient_email(String patient_email) {
+		this.patient_email = patient_email;
+	}
+
+	public String getTest_name() {
+		return test_name;
+	}
+
+	public void setTest_name(String test_name) {
+		this.test_name = test_name;
+	}
+
+	public String getAppointment_date() {
+		return appointment_date;
+	}
+
+	public void setAppointment_date(String appointment_date) {
+		this.appointment_date = appointment_date;
+	}
+    
+    
+
+}
